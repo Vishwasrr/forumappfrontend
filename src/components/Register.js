21f292f1af857bbe1c94525038378e234d162826
vishwasrr <vishwasrvibhu@gmail.com>
2022-07-03 23:26:12 +0530

    first commit

---
+++ src/components/Register.js
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from "react";
+import Button from "react-bootstrap/Button";
+import axios from "axios";
+import { url } from "../App";
+import { useNavigate } from "react-router-dom";
+import Form from "react-bootstrap/Form";
+import { Link } from "react-router-dom";
+
+function Register() {
+
+
+  let [name, setName] = useState("");
+  let [password, setPassword] = useState("");
+  let [email, setEmail] = useState("");
+  let navigate = useNavigate();
+  useEffect(() => {
+
+  }, []);
+
+  let handleSubmit = async () => {
+    let reqBody = {
+      name,
+      password,
+      email
+    };
+    let res = await axios.post(`${url}/signup`, reqBody);
+    if (res.data.statusCode === 200) {
+      navigate("/");
+    } else {
+      console.log(res.data.message);
+    }
+  };
+
+
+  return (
+    <>
+      <Form className="form-fields">
+        <Form.Group className="mb-3">
+          <h2 id="register-header">Register</h2>
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter Name"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <div><Button variant="primary" onClick={() => handleSubmit()}>
+          Submit
+        </Button>
+        </div>
+        <br></br>
+        <div >
+
+          <Link to='/Login'><button >Login?</button></Link>
+        </div>
+      </Form>
+
+    </>
+  );
+}
+
+export default Register;
