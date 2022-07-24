344c32f8141ce8d39304e25200c229891d95f493
Vishwas R <48607495+Vishwasrr@users.noreply.github.com>
2022-07-24 22:35:09 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,18 +4,39 @@ import Button from "react-bootstrap/Button";
 import axios from "axios";
 import { url } from "../App";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
 
 
+  var auth = sessionStorage.getItem("auth");
+  let navigate = useNavigate();
+  let handleLogoutClick = async () => {
+    sessionStorage.removeItem("auth");
+    navigate("/");
+
+  };
+
+  let handleLoginClick = async () => {
+    navigate("/Login");
+
+  };
 
   return (
     <div className="head-wrapper">
       <div className="head-title">StackOverFlow</div>
-      <div >
+      <div className="loginButton" >
+        {auth
+          ? <Button variant="primary" onClick={() => handleLogoutClick()}>
+            Logout
+          </Button>
 
+          : <Button variant="primary" onClick={() => handleLoginClick()}>
+            Login
+          </Button>
+
+        }
       </div>
-
       <div className="home-cart"></div>
     </div>
   );
